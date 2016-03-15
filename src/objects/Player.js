f09411fcc77bf6c081f0d0e0bd895accdaec8d77
Juan Ignacio Casal <ignaciocasal@gmail.com>
2016-03-15 15:29:50 -0300

    fixed player movement from any tap position

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -4,13 +4,36 @@ export default class Player extends Phaser.Sprite {
     super(game, x, y, 'player');
 
     this.anchor.set(0.5);
+
+    this.moving = false;
+    this.distanceX = 0;
+    this.distanceY = 0;
   }
 
   update () {
     var activePointer = this.game.input.activePointer;
-    if(Math.abs(this.x - activePointer.x) <= (this.width / 2) && Math.abs(this.y - activePointer.y) <= (this.height / 2)) {
+
+    if(false && Math.abs(this.x - activePointer.x) <= (this.width / 2) && Math.abs(this.y - activePointer.y) <= (this.height / 2)) {
       this.x = activePointer.x;
       this.y = activePointer.y;
+    }
+
+    if(!this.moving && activePointer.active){
+      this.moving = true;
+      this.distanceX = activePointer.x - this.x;
+      this.distanceY = activePointer.y - this.y;
+    }
+
+    if(this.moving){
+      if(activePointer.active){
+        this.x = activePointer.x - this.distanceX;
+        this.y = activePointer.y - this.distanceY;
+      }
+      else{
+        this.moving = false;
+        this.distanceX = 0;
+        this.distanceY = 0;
+      }
     }
   }
 
